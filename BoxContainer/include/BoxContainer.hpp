0266b6540bf1c85af495ab7c6e69af2aa545c909
vishal-ahirwar <vishlchoudhry0091@gmail.com>
2022-05-10 09:00:16 +0530

    Creating Container

---
+++ BoxContainer/include/BoxContainer.hpp
@@ -0,0 +1,15 @@
+#ifndef BOX_CONTAINER_HPP
+#define BOX_CONTAINER_HPP
+namespace BC
+{
+    class BoxContainer
+    {
+        public:
+            BoxContainer();
+        private:
+        protected:
+    };
+    
+};
+
+#endif
---
+++ BoxContainer/src/BoxContainer.cxx
@@ -0,0 +1,10 @@
+#include"../include/BoxContainer.hpp"
+
+namespace BC
+{
+    BoxContainer::BoxContainer()
+    {
+
+    };
+    
+}
